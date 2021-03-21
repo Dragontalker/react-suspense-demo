81f9e623db416d7da99e16a49cf31ad6ad9769bf
Richard <richard.yang.tong@gmail.com>
2021-03-21 15:18:06 -0400

    Added a wrapper function.

--- src/Api.js
+++ src/Api.js
@@ -9,6 +9,36 @@ export const fetchData = () => {
     }
 }
 
+const wrapPromise = (prommise) => {
+    // Set initial status
+    let status = 'pending';
+    // Store result
+    let result;
+    // Wait for promise
+    let suspender = promise.then(
+        res => {
+            status = 'success',
+            result = res;
+        },
+        err => {
+            status = 'error',
+            result = err
+        }
+    );
+
+    return {
+        read() {
+            if (status === 'pending') {
+                throw suspender;
+            } else if (status === 'error') {
+                throw result;
+            } else if (status === 'success') {
+                return result;
+            }
+        }
+    }
+}
+
 const fetchUser = () => {
     console.log('Featching User...');
     return axios.get('https://jsonplaceholder.typicode.com/todos/1')
