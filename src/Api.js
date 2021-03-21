6cab1ab28abf83c9effd2ec434faca9b634d53c9
Richard <richard.yang.tong@gmail.com>
2021-03-21 15:09:56 -0400

    Added a get request.

--- src/Api.js
+++ src/Api.js
@@ -1,1 +1,8 @@
 import axios from 'axios';
+
+const fetchUser = () => {
+    console.log('Featching User...');
+    return axios.get('https://jsonplaceholder.typicode.com/todos/1')
+        .then(res => res.data)
+        .catch(err => console.log(err));
+};
