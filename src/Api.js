5a202b3474e320b9dbd7cf5e788f95704ea9b217
Richard <richard.yang.tong@gmail.com>
2021-03-21 15:14:47 -0400

    Added an export function.

--- src/Api.js
+++ src/Api.js
@@ -1,5 +1,14 @@
 import axios from 'axios';
 
+export const fetchData = () => {
+    const userPromise = fetchUser();
+    const postsPromise = fetchPosts();
+    return {
+        users: wrapPromise(userPromise),
+        posts: wrapPromise(postsPromise)
+    }
+}
+
 const fetchUser = () => {
     console.log('Featching User...');
     return axios.get('https://jsonplaceholder.typicode.com/todos/1')
